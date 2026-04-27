9f76eae3c05490638c0b6038f8ae69e093ec4420
izhank216 <izhank216@gmail.com>
2026-04-27 16:26:20 +0500

    Update venue.js

--- venue.js
+++ venue.js
@@ -1,109 +1,107 @@
-// requirements
-var express	= require('express'),
-	app		= express(),
-	server	= require('http').createServer(app),
-	io		= require('socket.io').listen(server),
-	walk	= require('walk'),
-	colors	= require('colors');
-
-// reduce logging
-io.set('log level', 1);
+var WebSocket = require('ws'),
+    fs        = require('fs-extra'),
+    colors    = require('colors'),
+    LZString  = require('lz-string'),
+    EventEmitter = require('events'),
+    path      = require('path');
 
 function venue(data) {
-	console.log("   info  - ".cyan + "venue started");
+    console.log("   info   - ".cyan + "venue instance created");
 
-	var that = this;
+    var that = this;
+    EventEmitter.call(this);
 
-	if(data !== undefined) {
-		this.title	= (data.title === undefined ? "VenueFM" : data.title);
-		this.path	= (data.path === undefined ? __dirname + "/music" : data.path);
-		this.port	= (data.port === undefined ? 80 : data.port);
-		this.res	= (data.res === undefined ? __dirname : data.res);
-		this.css	= (data.css === undefined ? (__dirname + '/default.css') : ( data.res !== undefined ? data.res + "/" : "" ) + data.css);
-	}
+    if (data !== undefined) {
+        this.title = (data.title === undefined ? "VenueFM" : data.title);
+        this.path  = (data.path === undefined ? path.join(__dirname, "music") : data.path);
+        this.port  = 4821; 
+    }
 
-	this.can	= [];
-	this.cant	= [];
+    this.can     = [];
+    this.cant    = [];
+    this.current = -1;
 
-	this.current = -1;
+    this.on("broadcast", function(wss, payload) {
+        const compressedPayload = LZString.compressToUTF16(JSON.stringify(payload));
+        
+        wss.clients.forEach(function(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(compressedPayload);
+            }
+        });
+    });
 
-	this.launch = function() {
-		console.log("   info  - ".cyan + "Started to find files in directory '" + this.path + "'");
+    this.startWsServer = function() {
+        console.log("   info   - ".cyan + "Started to find files in directory '" + this.path + "'");
 
-		that.walker = walk.walk(this.path, { followLinks: false });
+        fs.walk(this.path)
+            .on('data', function(item) {
+                if (item.stats.isFile()) {
+                    var ext = path.extname(item.path).toLowerCase();
+                    if (ext === ".mp3" || ext === ".ogg" || ext === ".wav") {
+                        var relativePath = item.path.replace(that.path, "");
+                        that.can.push(relativePath);
+                    }
+                }
+            })
+            .on('end', function() {
+                console.log("   ok     - ".green + "Found the following number of files: " + that.can.length);
 
-		that.walker.on('file', function(root, stat, next) {
-			var crap = stat.name.split("."),
-				type = crap[crap.length - 1];
+                const wss = new WebSocket.Server({ port: that.port });
 
-			if(type === "mp3" || type === "ogg" || type === "wav")
-				that.can.push(root.replace(that.path, "") + "/" + stat.name);
+                console.log("   info   - ".cyan + "WS Server started, listening on port " + that.port);
 
-			next();
-		});
+                wss.on('connection', function connection(ws) {
+                    const handshakeData = { title: that.title, event: 'handshake' };
+                    ws.send(LZString.compressToUTF16(JSON.stringify(handshakeData)));
 
-		that.walker.on('end', function() {
-			console.log("   ok    - ".green + "Found the following number of files: " + that.can.length);	
+                    ws.on('message', function incoming(message) {
+                        const decompressed = LZString.decompressFromUTF16(message);
+                        let parsed;
+                        
+                        try {
+                            parsed = JSON.parse(decompressed);
+                        } catch(e) {
+                            return;
+                        }
 
-			app.use( express.static(that.path) );
+                        if (parsed.event === 'handshake' || parsed.event === 'next') {
+                            that.nextMusic(wss);
+                        }
+                    });
+                });
 
-			app.use('/resource', express.static(that.res));
+                that.emit('ready');
+            });
+    };
 
-			app.get('/', function (req, res) {
-				res.sendfile(__dirname + '/index.html');
-			});
+    this.nextMusic = function(wss) {
+        if (that.can.length === 0 && that.cant.length === 0) return;
 
-			app.get('/venue/client.js', function (req, res) {
-				res.sendfile(__dirname + '/client.js');
-			});
+        var old = that.can.splice(that.current, 1);
 
-			app.get('/venue/style.css', function (req, res) {
-				res.sendfile(that.css);
-			});
+        if (that.can.length === 0) {
+            that.can = that.cant;
+            that.current = that.can.length - 1;
+            that.can.push(old);
+            that.cant = [];
+        } else {
+            if(old.length > 0) that.cant.push(old);
+            that.current = Math.floor(Math.random() * that.can.length);
+        }
 
-			server.listen(that.port);
+        const trackPath = that.can[that.current];
+        console.log("   info   - ".cyan + "Broadcasting " + trackPath);
 
-			console.log("   info  - ".cyan + "Server started, listening on port " + that.port);
-
-			io.sockets.on('connection', function(socket){
-				socket.emit('handshake', {
-					title: that.title
-				});
-
-				socket.on('handshake', function(socket){
-					that.nextMusic();
-				});
-
-				socket.on('next', function(socket) {
-					that.nextMusic();
-				});
-			});
-		});
-	};
-
-	this.nextMusic = function() {
-		var old = that.can.splice(that.current, 1);
-
-		if(that.can.length === 0) {
-			that.can = that.cant;
-
-			that.current = that.can.length;
-
-			that.can.push(old);
-			that.cant = [];
-		} else {
-			that.cant.push(old);
-
-			that.current = Math.floor(Math.random() * that.can.length);
-		}
-
-		console.log("   info  - ".cyan + "Broadcasting " + that.can[that.current].split(-1)[0]);
-
-		io.sockets.emit('pop', { path: that.can[that.current] });
-	}
+        this.emit("broadcast", wss, { event: 'pop', path: trackPath });
+        this.emit('play', trackPath);
+    }
 }
 
-exports.launch = function(data) {
-	var v = new venue(data)
-	v.launch();
+require('util').inherits(venue, EventEmitter);
+
+exports.startWsServer = function(data) {
+    var v = new venue(data);
+    v.startWsServer();
+    return v;
 };
