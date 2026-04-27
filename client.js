f5db5f0a31c116aab0de622832d6f9632501769a
izhank216 <izhank216@gmail.com>
2026-04-27 16:27:23 +0500

    Update client.js

--- client.js
+++ client.js
@@ -1,52 +1,73 @@
-var socket = io.connect(),
+var socket = new WebSocket("ws://" + window.location.hostname + ":4821"),
 	player = document.getElementById("player");
 
 var title;
 
-socket.on('handshake', function (data) {
+socket.onopen = function() {
 	console.log("connected");
-	title = data.title;
-	document.title = data.title;
-	document.getElementsByTagName("h1")[0].innerHTML = data.title;
-	finishLoading();
-	socket.emit('handshake', { });
-});
+};
 
-socket.on('pop', function (data) {
-	console.log("Received new music: " + data.path);
+socket.onmessage = function(event) {
+	var decompressed = LZString.decompressFromUTF16(event.data);
+	var data;
 
-	var songTitle = data.path.replace("/", "").replace(/\//g, ' &mdash; ').split(".mp3")[0].split(".ogg")[0].split(".wav")[0]
+	try {
+		data = JSON.parse(decompressed);
+	} catch (e) {
+		console.error("Failed to parse message", e);
+		return;
+	}
 
-	document.title = songTitle + " at " + title;
-	document.getElementById("title").innerHTML = songTitle;
+	if (data.event === 'handshake') {
+		title = data.title;
+		document.title = data.title;
+		document.getElementsByTagName("h1")[0].innerHTML = data.title;
+		finishLoading();
+		
+		var response = JSON.stringify({ event: 'handshake' });
+		socket.send(LZString.compressToUTF16(response));
+	}
 
-	player.src = data.path;
-});
+	if (data.event === 'pop') {
+		console.log("Received new music: " + data.path);
 
-socket.on('error', function (data) {
-	console.log("[error] " + (data || 'Unknown error'));
+		var songTitle = data.path.replace("/", "").replace(/\//g, ' &mdash; ').split(".mp3")[0].split(".ogg")[0].split(".wav")[0];
 
+		document.title = songTitle + " at " + title;
+		document.getElementById("title").innerHTML = songTitle;
+
+		player.src = window.location.protocol + "//" + window.location.hostname + data.path;
+	}
+};
+
+socket.onerror = function(error) {
+	console.log("[error] WebSocket Error");
 	alert("There has been an error with the server, maybe it's down?");
-});
+};
+
+socket.onclose = function() {
+	console.log("Connection closed");
+};
 
 var state = false;
 
 function pause() {
-	if(state) {
+	if (state) {
 		document.getElementById("pause").innerHTML = "play";
 		player.pause();
 	} else {
 		document.getElementById("pause").innerHTML = "pause";
 		player.play();
 	}
-
-	state = ! state;
+	state = !state;
 }
 
 function next() {
 	document.getElementById("pause").innerHTML = "pause";
 	console.log("Sending order to change music");
-	socket.emit('next', { });
+	
+	var message = JSON.stringify({ event: 'next' });
+	socket.send(LZString.compressToUTF16(message));
 
 	state = true;
 }
@@ -54,13 +75,14 @@ function next() {
 function finishLoading() {
 	document.getElementById("content").style.display = "block";
 	document.getElementById("loading").style.display = "none";
-
 	state = true;
 }
 
 window.onload = function() {
 	document.getElementById("next").onclick = next;
 	document.getElementById("pause").onclick = pause;
-	player.addEventListener('loadedmetadata', player.play, false);
+	player.addEventListener('loadedmetadata', function() {
+		player.play();
+	}, false);
 	player.addEventListener("ended", next, false);
-}
+};
